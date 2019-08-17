340aa9203050e76a9719084507f71c13dcd13383
Wesley Zhang <zhanglei2709@163.com>
2019-08-18 03:16:09 +0800

    init

---
+++ src/main/java/org/geek/marathon/Application.java
@@ -0,0 +1,7 @@
+package org.geek.marathon;
+
+class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/geek/marathon/Calculator.java
@@ -0,0 +1,7 @@
+package org.geek.marathon;
+
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
